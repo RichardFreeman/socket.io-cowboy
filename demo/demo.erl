4180490590af2a8a8f866fc790b6dee668aa0c05
Richard Huang <recurlamlisp@gmail.com>
2014-12-14 20:34:01 +0800

    demo and jsx fix

--- demo/demo.erl
+++ demo/demo.erl
@@ -6,6 +6,7 @@
 
 start() ->
     ok = application:start(sasl),
+    ok = application:start(asn1),
     ok = application:start(crypto),
     ok = application:start(public_key),
     ok = application:start(ssl),
